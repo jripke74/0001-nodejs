90fee7500f4d502e1191bf3e2413fdb4f9833752
Jeff Ripke <jripke74@gmail.com>
2023-08-22 09:54:59 -0700

    add detail view

--- controllers/shop.js
+++ controllers/shop.js
@@ -12,10 +12,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    console.log(product);
+  Product.findById(prodId, (product) => {
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.pageTitle,
+      path: "/products",
+    });
   });
-  res.redirect("/");
 };
 
 exports.getIndex = (req, res, next) => {
