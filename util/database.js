9a0b93544b96ad8260badc779f67bfea5a2c08df
Jeff Ripke <jripke74@gmail.com>
2023-08-28 13:05:16 -0700

    add sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("products", "root", "58585858", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "products",
-  password: "58585858",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
