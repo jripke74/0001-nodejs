1111405108f69ee7d65907d121fb1a65f9f90651
Jeff Ripke <jripke74@gmail.com>
2023-08-29 09:00:19 -0700

    add findByPk()

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,10 +4,10 @@ const Cart = require("../models/cart");
 exports.getProducts = (req, res, next) => {
   Product.findAll()
     .then((products) => {
-      res.render("shop/index", {
+      res.render("shop/product-list", {
         prods: products,
         pageTitle: "Shop",
-        path: "/",
+        path: "/products",
       });
     })
     .catch((err) => {
@@ -17,10 +17,10 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
+  Product.findByPk(prodId)
+    .then((product) => {
       res.render("shop/product-detail", {
-        product: product[0],
+        product: product,
         pageTitle: product.title,
         path: "/products",
       });
@@ -42,13 +42,11 @@ exports.getIndex = (req, res, next) => {
     });
 };
 
-exports.getCart = (req, res, next) => {
-
-};
+exports.getCart = (req, res, next) => {};
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) => {
+  Product.findByPk(prodId, (product) => {
     Cart.addProduct(prodId, product.price);
   });
   res.redirect("/cart");
@@ -56,7 +54,7 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) => {
+  Product.findByPk(prodId, (product) => {
     Cart.deleteProduct(prodId, product.price);
     res.redirect("/cart");
   });
